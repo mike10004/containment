816f7af9766b0bd3f9bbcf29c8d9388d6453528e
Mike Chaberski <mchaberski@novetta.com>
2019-11-06 11:12:55 -0500

    add javadoc

--- containment-api/src/main/java/io/github/mike10004/containment/StartedContainer.java
+++ containment-api/src/main/java/io/github/mike10004/containment/StartedContainer.java
@@ -22,10 +22,35 @@ public interface StartedContainer extends ActionableContainer, AutoCloseable {
     @Override
     void close() throws ContainmentException;
 
+    /**
+     * Fetch ports of this container that may be bound to host ports.
+     * @return a list of possibly-bound container ports
+     * @throws ContainmentException if port list could not be fetched
+     */
     List<ContainerPort> fetchPorts() throws ContainmentException;
 
+    /**
+     * Starts following the standard output stream of the container process.
+     * This method returns immediately, and the consumer accepts frames of output
+     * asynchronously.
+     * Frames of the output stream usually contain a single line of text output.
+     * @param consumer the consumer
+     * @param <C> consumer type
+     * @return the argument consumer
+     * @throws ContainmentException if container process standard output stream could not be followed
+     */
     <C extends Consumer<? super byte[]>> C followStdout(C consumer) throws ContainmentException;
 
+    /**
+     * Starts following the standard error stream of the container process.
+     * This method returns immediately, and the consumer accepts frames of output
+     * asynchronously.
+     * Frames of the output stream usually contain a single line of text output.
+     * @param consumer the consumer
+     * @param <C> consumer type
+     * @return the argument consumer
+     * @throws ContainmentException if container process standard error stream could not be followed
+     */
     <C extends Consumer<? super byte[]>> C followStderr(C consumer) throws ContainmentException;
 
 }
